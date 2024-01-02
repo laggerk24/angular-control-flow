f36e4245b2d1237cc6de3cd79a7cc095451df08a
ICEPOR\Aman.Kumar2 <aman.kumar2@ivycomptech.com>
2024-01-02 17:07:48 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { DataService } from './data/data.service';
+import { UserListComponent } from './user-list/user-list.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ PostListComponent, UserListComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  #dataProvider = inject(DataService);
+
+  data = toSignal(this.#dataProvider.data$);
+
+  onFetch() {
+    this.#dataProvider.fetch();
+  }
+}
